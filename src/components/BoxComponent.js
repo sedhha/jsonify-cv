905438a4f083102f36eb37653f33d147e0bc22d3
SHIVAM SAHIL <activity.schoolsh2@gmail.com>
2020-09-04 08:37:18 +0530

    First commit

---
+++ src/components/BoxComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function BoxComponent()
+{
+    return(
+        <div className="Primary-Box">
+        <div className="Your-Profile-Photo">
+        </div>
+        </div>
+    );
+}
